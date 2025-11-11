80d7ab1a22f6d098cd92cfc79ef0dfb83173ef0b
igdev <dorultanianos@gmail.com>
2025-11-11 14:18:30 +0200

    feat(cli): ensure list is compatible with uniswap schema

--- packages/cli/scripts/find-sources.ts
+++ packages/cli/scripts/find-sources.ts
@@ -1,10 +1,19 @@
-import type {TokenList} from "@tokenlist-builder/core";
+import {TokenList, tokenListSchema} from "@tokenlist-builder/core";
 import {LIST_SOURCES} from '../constants';
+import Ajv from 'ajv';
+
+
+const ajv = new Ajv();
+const validator = ajv.addSchema(tokenListSchema);
 
 async function main(sources: string[]) {
   return Promise.all(sources.map<Promise<TokenList>>(async (item) => {
     const res = await fetch(LIST_SOURCES[item]);
-    return res.json() as Promise<TokenList>;
+    const data = await res.json() as TokenList;
+    if (!validator.validate(tokenListSchema, data)) {
+      throw new Error(`The list ${item} from ${LIST_SOURCES[item]} is invalid.`);
+    }
+    return data;
   }));
 }
 
