4be1534c2d28f208f8a8e0d64feadb0e1b96a475
igdev <dorultanianos@gmail.com>
2025-11-11 12:08:36 +0200

    refactor(core): re-export uniswap schema from package instead of manually copying the schema

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,7 @@
-import tokenListSchema from "../schemas/tokenlist.schema.json";
 import tokenListRulesSchema from "../schemas/tokenlist-rules.schema.json";
-export type {TokenList} from "@uniswap/token-lists";
 
-export {tokenListSchema, tokenListRulesSchema};
+export type {TokenList} from "@uniswap/token-lists";
+export {schema as tokenListSchema} from "@uniswap/token-lists";
+
+export {tokenListRulesSchema};
 
