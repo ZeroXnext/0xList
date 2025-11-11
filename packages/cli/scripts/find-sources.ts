1d4dafb206c0aa8f6e358cb1780aaf0a287e352f
igdev <dorultanianos@gmail.com>
2025-11-11 13:46:32 +0200

    feat(cli): impl find token list sources script

--- packages/cli/scripts/find-sources.ts
+++ packages/cli/scripts/find-sources.ts
@@ -1,10 +1,10 @@
 import type {TokenList} from "@tokenlist-builder/core";
 import {LIST_SOURCES} from '../constants';
 
-async function main() {
-  return Promise.all(Object.keys(LIST_SOURCES).map<Promise<TokenList[]>>(async (item) => {
+async function main(sources: string[]) {
+  return Promise.all(sources.map<Promise<TokenList>>(async (item) => {
     const res = await fetch(LIST_SOURCES[item]);
-    return res.json() as Promise<TokenList[]>;
+    return res.json() as Promise<TokenList>;
   }));
 }
 
