9ec15dbba6421822e9a43d308c87deeca0d02550
igdev <dorultanianos@gmail.com>
2025-11-12 09:07:20 +0200

    feat(cli): add new list source

--- packages/cli/constants.ts
+++ packages/cli/constants.ts
@@ -24,7 +24,8 @@ export const CHAINS_MAPPING: Record<number, Chain> = {
 
 export const LIST_SOURCES: Record<string, string> = {
   "uniswap": "https://raw.githubusercontent.com/jab416171/uniswap-pairtokens/master/uniswap_pair_tokens.json",
-  "aawave": "https://wispy-bird-88a7.uniswap.workers.dev/?url=http://tokenlist.aave.eth.link"
+  "aawave": "https://wispy-bird-88a7.uniswap.workers.dev/?url=http://tokenlist.aave.eth.link",
+  "1inch": "https://wispy-bird-88a7.uniswap.workers.dev/?url=http://tokens.1inch.eth.link",
 };
 
 export const DEFAULT_NETWORK_TYPES = ["testnet", "mainnet"]
