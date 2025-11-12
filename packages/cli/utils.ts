40053267877dc7281af3d94acfcdf25df87f1a3b
igdev <dorultanianos@gmail.com>
2025-11-12 09:42:49 +0200

    feat(cli): finish implementation of partitionTokenList

--- packages/cli/utils.ts
+++ packages/cli/utils.ts
@@ -1,6 +1,7 @@
 import {TokenList, tokenListSchema} from "@tokenlist-builder/core";
+import {DEFAULT_LIST_VERSION} from './constants';
 
-export function partitionArray<T>(array: T[], maxItems:number): T[][] {
+export function partitionArray<T>(array: T[], maxItems: number): T[][] {
   if (maxItems <= 0) throw new Error("maxItems must be greater than 0");
 
   const result: T[][] = [];
@@ -10,18 +11,18 @@ export function partitionArray<T>(array: T[], maxItems:number): T[][] {
   return result;
 }
 
-export function partitionTokenList(tokenList: TokenList): TokenList[] {
+export function partitionTokenList(tokenList: Omit<TokenList, "version" | "timestamp">, version: TokenList['version'] = DEFAULT_LIST_VERSION, timestamp = new Date().toLocaleTimeString()): TokenList[] {
   // Copy metadata from the list and the partitioned tokens
   const tokens = partitionArray(tokenList.tokens, tokenListSchema.properties.tokens.maxItems);
   let tokenLists: TokenList[] = [];
   let i = 0;
   for (const _tokens of tokens) {
     i++;
-    tokenLists.push({
+    tokenLists.push(initializeTokenList({
       ...tokenList,
       name: `${tokenList.name}${i}`, // use unique name because this will be the filename
       tokens: _tokens
-    })
+    }, version, timestamp));
   }
 
   return tokenLists;
@@ -45,11 +46,7 @@ export function initializeTokenList({
   keywords: [],
   timestamp: new Date().toISOString(),
   tokens: [],
-}, version: TokenList['version'] = {
-  major: 1,
-  patch: 0,
-  minor: 0,
-}, timestamp = new Date().toLocaleTimeString()): TokenList {
+}, version: TokenList['version'] = DEFAULT_LIST_VERSION, timestamp = new Date().toLocaleTimeString()): TokenList {
   return {
     name,
     logoURI,
