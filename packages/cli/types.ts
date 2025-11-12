154c5c26472ac2b0732e0569733078bc63d69b32
igdev <dorultanianos@gmail.com>
2025-11-12 10:38:51 +0200

    feat(cli): impl mutable type util & make MutableTokenList alias

--- packages/cli/types.ts
+++ packages/cli/types.ts
@@ -6,3 +6,9 @@ export interface Chain {
 }
 
 export type MappedTokenLists = Map<string, Map<string, TokenList>>;
+
+export type Mutable<T> = {
+  -readonly [P in keyof T]: T[P];
+};
+
+export type MutableTokenList = Mutable<TokenList>;
