b139a1f54685dab686ae1634aa2d6574c663d6e7
igdev <dorultanianos@gmail.com>
2025-11-12 11:21:46 +0200

    feat(cli): implement unique path for token list

--- packages/cli/scripts/output.ts
+++ packages/cli/scripts/output.ts
@@ -1,18 +1,19 @@
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 import {MappedTokenLists} from '../types';
+import {slugify} from '../utils';
+import {getUniqueFilePath} from '../helpers';
 
-function main(outputDir: string, tokenListMapping: MappedTokenLists) {
+function main(outputDir: string, tokenListMapping: MappedTokenLists, override = false) {
   if (!fs.existsSync(outputDir)) {
-    fs.mkdirSync(outputDir, { recursive: true });
+    fs.mkdirSync(outputDir, {recursive: true});
   }
 
   for (const [chainType, chainMap] of tokenListMapping.entries()) {
     for (const [chainName, tokenList] of chainMap.entries()) {
       const dirPath = path.join(outputDir, chainType, chainName);
-      const filePath = path.join(dirPath, `${tokenList.name}.json`);
-
-      fs.mkdirSync(dirPath, { recursive: true });
+      const filePath = getUniqueFilePath(dirPath, slugify(tokenList.name));
+      fs.mkdirSync(dirPath, {recursive: true});
       fs.writeFileSync(filePath, JSON.stringify(tokenList, null, 2), 'utf-8');
     }
   }
