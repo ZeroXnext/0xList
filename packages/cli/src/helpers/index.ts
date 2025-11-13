8c585907cf10da403e6b72cce34b02a2d36641b4
igdev <dorultanianos@gmail.com>
2025-11-13 09:08:55 +0200

    refactor(cli): make partitioning its own helper file

--- packages/cli/src/helpers/index.ts
+++ packages/cli/src/helpers/index.ts
@@ -1,7 +1,6 @@
 import {TokenList, tokenListSchema} from '@tokenlist-builder/core';
 import {DEFAULT_LIST_LOGO_URI, DEFAULT_LIST_VERSION, DEFAULT_TOKEN_LIST_NAME} from '@constants';
 import {MutableTokenList} from '@types';
-import {partitionArray} from '@utils';
 import path from 'node:path';
 import * as fs from 'node:fs';
 import Ajv from 'ajv';
@@ -10,29 +9,9 @@ import classify from './classify';
 import load from './load';
 import resolve from './resolve';
 import output from './output';
+import partition from './partition';
 
 export {classify, load, resolve, output};
-
-export function partitionTokenList(tokenList: Omit<TokenList, "version" | "timestamp">,
-                                   version: TokenList['version'] = DEFAULT_LIST_VERSION,
-                                   defaultTokenListName = DEFAULT_TOKEN_LIST_NAME): MutableTokenList[] {
-  // Copy metadata from the list and the partitioned tokens
-  const tokens = partitionArray(tokenList.tokens, tokenListSchema.properties.tokens.maxItems);
-  let tokenLists: TokenList[] = [];
-
-  let i = 0;
-  for (const _tokens of tokens) {
-    const name = `${tokenList.name}${i}`;
-    i++;
-    tokenLists.push(initializeTokenList({
-      ...tokenList,
-      name: name.length > tokenListSchema.properties.name.maxLength ? `${defaultTokenListName} ${i}` : name,
-      tokens: _tokens
-    }, version));
-  }
-
-  return tokenLists;
-}
 
 export function initializeTokenList({
                                       name,
@@ -87,7 +66,7 @@ export function normalizeTokenList(data: MutableTokenList, defaultTokenListName 
   addFormats(ajv);
 
   data.tokens = normalizeTokens(data.tokens);
-  const maybePartitionedLists = (data as TokenList).tokens.length > tokenListSchema.properties.tokens.maxItems ? partitionTokenList(data, defaultVersion, defaultTokenListName) : [data as MutableTokenList];
+  const maybePartitionedLists = (data as TokenList).tokens.length > tokenListSchema.properties.tokens.maxItems ? partition(data, defaultVersion, defaultTokenListName) : [data as MutableTokenList];
 
   for (let list of maybePartitionedLists) {
     if (list.name.length > tokenListSchema.properties.name.maxLength) {
