7917b4782ac2cb75238b52a19badc997643364da
igdev <dorultanianos@gmail.com>
2025-11-15 14:44:40 +0200

    feat(cli): impl fetch external helper

---
+++ packages/cli/src/helpers/fetch-external.ts
@@ -0,0 +1,17 @@
+import {TokenList} from "@tokenlist-builder/core";
+
+export default function fetchExternal(sources: string[]) {
+  return Promise.all(sources.map(async (source) => {
+    try {
+      const res = await fetch(source);
+      if (!res.ok) {
+        console.warn("Failed to load source: ", source);
+        return null;
+      }
+      return await res.json() as TokenList;
+    } catch (error) {
+      console.warn("Failed to load source: ", source);
+      return null;
+    }
+  }));
+}
