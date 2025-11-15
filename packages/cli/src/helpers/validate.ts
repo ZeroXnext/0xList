e61c89575822103db2d8e1eaa5f46ea36d4c1d5d
igdev <dorultanianos@gmail.com>
2025-11-15 14:44:24 +0200

    feat(cli): impl validate helper

---
+++ packages/cli/src/helpers/validate.ts
@@ -0,0 +1,11 @@
+import Ajv from 'ajv';
+import {TokenList, tokenListSchema} from '@tokenlist-builder/core';
+import addFormats from 'ajv-formats';
+
+export default function validate(tokenList: TokenList) {
+  const ajv = new Ajv();
+  const validator = ajv.addSchema(tokenListSchema);
+  addFormats(ajv);
+
+  return [validator.validate(tokenListSchema, tokenList), validator.errors?.map(item => `${item.instancePath}:${item.message}`)?.join(',\n')];
+}
