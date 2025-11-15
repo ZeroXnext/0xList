461e9d09e936eb216604535c1b4ffc41d47b5f9b
igdev <dorultanianos@gmail.com>
2025-11-15 14:52:33 +0200

    refactor(cli): remove inexistent resolve ref

--- packages/cli/src/helpers/index.ts
+++ packages/cli/src/helpers/index.ts
@@ -1,8 +1,7 @@
 import classify from './classify';
 import load from './load-flat-local';
-import resolve from './resolve';
 import output from './output';
 import initializeTokenList from './initialize-token-list';
 import getUniqueFilePath from './unique-path';
 import bump from './bump';
-export {classify, load, resolve, output, initializeTokenList, getUniqueFilePath, bump};
+export {classify, load, output, initializeTokenList, getUniqueFilePath, bump};
