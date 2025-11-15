81f5e7a167cc449433f7edc73542d99433f1a444
igdev <dorultanianos@gmail.com>
2025-11-15 14:46:50 +0200

    feat(cli)!: implement seen set for loader

--- packages/cli/src/helpers/load-flat-local.ts
+++ packages/cli/src/helpers/load-flat-local.ts
@@ -1,8 +1,18 @@
 import fs from 'node:fs';
 import {TokenList} from '@tokenlist-builder/core';
 import path from 'node:path';
+import {SeenKey} from '@types';
+import {CHAINS_MAPPING} from '@constants';
 
-export default function loadFlatLocal(outputDir: string): Map<string, TokenList> {
+export default function load(outputDir: string): [Map<string, TokenList>, Set<SeenKey>] {
   const [, , ...paths] = fs.readdirSync(outputDir, {recursive: true});
-  return new Map<string, TokenList>(paths.map(key => [path.join(outputDir, key.toString()), JSON.parse(fs.readFileSync(path.join(outputDir, key.toString()), 'utf-8')) as TokenList]));
+  const seen = new Set<SeenKey>();
+  return [new Map<string, TokenList>(paths.map(key => {
+    const list = JSON.parse(fs.readFileSync(path.join(outputDir, key.toString()), 'utf-8')) as TokenList;
+    for (let {address, chainId} of list.tokens) {
+      const {name, type} = CHAINS_MAPPING[chainId];
+      seen.add(`${type}:${name}:${address}`);
+    }
+    return [path.join(outputDir, key.toString()), list];
+  })), seen];
 }
