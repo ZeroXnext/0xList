23715e2965983c3dda4b12e74eab346605b483f7
igdev <dorultanianos@gmail.com>
2025-11-15 16:08:15 +0200

    fix(cli): output script was not also making the possible inexistent directory

--- packages/cli/src/helpers/output-basic.ts
+++ packages/cli/src/helpers/output-basic.ts
@@ -2,5 +2,8 @@ import {TokenList} from "@tokenlist-builder/core";
 import fs from 'node:fs';
 
 export default function outputBasic(output: string, tokenList: TokenList) {
-  fs.writeFileSync(output, JSON.stringify(tokenList, null, 2), 'utf-8');
+  const dir = output.split('/');
+  dir.pop()
+  fs.mkdirSync(dir.join('/'), { recursive: true });
+  fs.writeFileSync(output, JSON.stringify(tokenList, null, 2), {encoding: "utf8"});
 }
