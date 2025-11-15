137d3d54a598e47cb29bbdbb80f78a55d8abfb96
igdev <dorultanianos@gmail.com>
2025-11-15 14:55:28 +0200

    refactor(cli): remove unused output helper

--- packages/cli/src/helpers/index.ts
+++ packages/cli/src/helpers/index.ts
@@ -1,6 +1,6 @@
 import classify from './classify';
 import load from './load-flat-local';
-import output from './output';
 import initializeTokenList from './initialize-token-list';
 import bump from './bump';
-export {classify, load, output, initializeTokenList, bump};
+
+export {classify, load, initializeTokenList, bump};
