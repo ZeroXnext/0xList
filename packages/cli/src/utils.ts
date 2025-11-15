d92a983b4ef9db498bb23405ba0473d92f38bcdd
igdev <dorultanianos@gmail.com>
2025-11-15 16:47:05 +0200

    refactor(cli): remove unused partitionArray util

--- packages/cli/src/utils.ts
+++ packages/cli/src/utils.ts
@@ -1,14 +1,3 @@
-
-export function partitionArray<T>(array: T[], maxItems: number): T[][] {
-  if (maxItems <= 0) throw new Error("maxItems must be greater than 0");
-
-  const result: T[][] = [];
-  for (let i = 0; i < array.length; i += maxItems) {
-    result.push(array.slice(i, i + maxItems));
-  }
-  return result;
-}
-
 export function slugify(str: string): string {
   return str
       .normalize("NFD")                 // decompose accents/diacritics
