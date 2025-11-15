abf29ada47ee4cfe7234c467944d62d205f4e165
igdev <dorultanianos@gmail.com>
2025-11-15 12:01:45 +0200

    feat(cli): complete basic implementation of bump command

--- packages/cli/src/commands/bump.ts
+++ packages/cli/src/commands/bump.ts
@@ -1,8 +1,38 @@
 import {Entry} from '@types';
+import * as childProcess from 'node:child_process';
+import loadFlatLocal from '../helpers/load-flat-local';
+import {TokenList} from '@tokenlist-builder/core';
+import autoVersion from '../helpers/auto-version';
+import outputBasic from '../helpers/output-basic';
+import {parseGitRemoteUrl} from '@utils';
+
 
 function addBumpCommand(entry: Entry): void {
-  entry.command("bump", "It auto-updates the version of the token list, according to rules", () => {}, () => {
+  entry.command("bump", "It auto-updates the version of the token list, according to rules", () => {
+  }, async (argv) => {
+    const {output: outputDir} = argv;
+    const stder = childProcess.execSync("git remote get-url origin", {encoding: 'utf8'});
+    const repo = parseGitRemoteUrl(stder);
+    const baseUrl = `https://raw.githubusercontent.com/${repo.username}/${repo.repo}`;
 
+    const localLists = loadFlatLocal(outputDir);
+
+    for (const [key, localList] of localLists.entries()) {
+      try {
+        const res = await fetch(`${baseUrl}${key}`);
+        if (res.status === 404) {
+          // ignore, this means that the list is new
+          continue;
+        }
+
+        const remoteList = await res.json() as TokenList;
+
+        autoVersion(remoteList, localList);
+        outputBasic(outputDir, localList);
+      } catch {
+        // ignore, it means that the list is not new
+      }
+    }
   });
 }
 
