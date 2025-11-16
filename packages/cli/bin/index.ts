cedb4c83f7bf39ef8b84fbbd09edbfa3a1cfd36e
igdev <dorultanianos@gmail.com>
2025-11-16 12:18:19 +0200

    feat(cli): add aggregate command to entry

--- packages/cli/bin/index.ts
+++ packages/cli/bin/index.ts
@@ -2,7 +2,9 @@
 import {entry} from '../src';
 import addBumpCommand from '../src/commands/bump';
 import addGenerateCommand from '../src/commands/generate';
+import addAggregate from '../src/commands/aggregate';
 
 addGenerateCommand(entry);
 addBumpCommand(entry);
+addAggregate(entry);
 entry.strictCommands().parse();
