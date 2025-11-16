58de80f67319ca8ec66d257ec0a8e118bc3bdb0e
igdev <dorultanianos@gmail.com>
2025-11-16 13:41:28 +0200

    feat(core): setup jest closes #4

---
+++ packages/core/jest.config.ts
@@ -0,0 +1,18 @@
+import {Config} from "jest";
+import {createDefaultPreset} from "ts-jest";
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+export default {
+  testEnvironment: "node",
+  testMatch: ["<rootDir>/**/**/.test.ts", "<rootDir>/**/*.test.ts"],
+  transform: {
+    ...tsJestTransformCfg,
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+      },
+    ],
+  },
+} as Config;
