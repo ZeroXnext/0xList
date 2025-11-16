14091c5401281345877bbb681e945dc79612f84c
igdev <dorultanianos@gmail.com>
2025-11-16 12:12:12 +0200

    feat(cli): implement build index helper and create new explicit paths types

---
+++ packages/cli/src/helpers/build-index.ts
@@ -0,0 +1,23 @@
+import {ListIndex, ListPath, ListURL} from '@types';
+import {TokenList} from "@tokenlist-builder/core";
+import fs from 'node:fs';
+import {timestamp} from '@utils';
+import path from 'node:path';
+
+export default function buildIndex(remoteBaseUrl: string, indexFullPath: string, listMap: Map<ListPath, TokenList>) {
+  // Directory path already exists, just build the index and write to the disk
+  const indexData: ListIndex = {
+    lists: [],
+    timestamp: timestamp()
+  };
+
+  // Assuming the base is already merged into the listPath
+  for (const [listPath, {tokens, ...list}] of listMap.entries()) {
+    const listUrl = path.join(remoteBaseUrl, listPath) as ListURL;
+    indexData.lists.push({
+      ...list,
+      contents: listUrl,
+    });
+  }
+  fs.writeFileSync(indexFullPath, JSON.stringify(indexData, null, 2), {encoding: "utf8"});
+}
--- packages/cli/src/types.ts
+++ packages/cli/src/types.ts
@@ -1,8 +1,14 @@
+import {TokenList} from "@tokenlist-builder/core";
 import {entry} from './index';
 
 export interface Chain {
   name: string;
   type: string;
+}
+
+export interface ListIndex {
+  timestamp: string; // representing last updated at
+  lists: Array<Omit<TokenList, 'tokens'> & Record<"contents", ListURL>>;
 }
 
 export type Mutable<T> = {
@@ -12,8 +18,21 @@ export type Mutable<T> = {
 
 export type Entry = typeof entry;
 
-// <root>/<network_type>/<chain_name>/<list-name>.json
+export type GHUserRawContentURL = `https://raw.githubusercontent.com`;
+
+export type IndexName = "index" | string
+
+// <base>/<network_type>/<chain_name>/<list-name>.json
 export type ListPath = `${string}/${string}/${string}/${string}.json`;
 
+//<base>/<network_type>/<"index" | name>.json
+export type IndexPath = `${string}/${string}/${IndexName | `/${IndexName}`}.json`;
+
 // <network_type>:<chain_id>:<token_address>
-export type SeenKey = `${string}:${string}:${string}`
+export type SeenKey = `${string}:${string}:${string}`;
+
+// <GHUserRawContentURL>/<basePath>/<network_type>/<name>.json
+// <GHUserRawContentURL>/<basePath>/<network_type>/<chain_name>/<"index" | name>.json
+export type IndexURL = `${GHUserRawContentURL}/${IndexPath}`;
+
+export type ListURL = `${GHUserRawContentURL}/${ListPath}`;
