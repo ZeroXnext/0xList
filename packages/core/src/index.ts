a07e1a51793854f19037e8e211c8bec0d53669b7
igdev <dorultanianos@gmail.com>
2025-11-16 21:11:28 +0200

    refactor(core): change tokenListRulesSchema -> configSchema

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,7 @@
-import tokenListRulesSchema from "../schemas/config.schema.json";
+import configSchema from "../schemas/config.schema.json";
 import tokenListIndexSchema from "../schemas/tokenlist-index.schema.json";
 
 export type {TokenList} from "@uniswap/token-lists";
 export * from "./types";
 export {schema as tokenListSchema} from "@uniswap/token-lists";
-export {tokenListRulesSchema, tokenListIndexSchema};
+export {configSchema, tokenListIndexSchema};
