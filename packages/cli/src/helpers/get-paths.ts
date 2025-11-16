4fb7249067f59e87c5fdd005d58a6db9d9b62378
igdev <dorultanianos@gmail.com>
2025-11-16 12:13:38 +0200

    docs(cli): add get-paths details in jsdocs format

--- packages/cli/src/helpers/get-paths.ts
+++ packages/cli/src/helpers/get-paths.ts
@@ -1,6 +1,11 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
+/**
+ * @param outputDir – the output directory
+ * @returns string[]
+ * @description A basic method that will retrieve the token lists paths excluding the index.
+ */
 export default function getPaths(outputDir: string) {
   return fs.readdirSync(outputDir, {recursive: true}).map(item => item.toString()).filter((p) => {
     try {
