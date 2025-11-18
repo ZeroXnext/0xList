837871e001c0fa2b5259c81056046a8ac7bcf8b7
igdev <dorultanianos@gmail.com>
2025-11-18 18:30:44 +0200

    refactor(core): make loader return a Promise<Config> explicit

--- packages/core/src/config/loader.ts
+++ packages/core/src/config/loader.ts
@@ -1,14 +1,15 @@
 import {Config} from '@types';
 import defaultConfig from './default';
 
-async function loader(configName = "0xlist.config") {
+async function loader(configName = "0xlist.config"): Promise<Config> {
   let config: Partial<Config>;
   try {
-    config = {...defaultConfig, ...(await import(configName) ?? {})};
+    config = {...defaultConfig, ...(await import(configName) ?? {}).default as Config};
   } catch (error) {
     config = defaultConfig;
   }
-  return config;
+
+  return config as Config;
 }
 
 export default loader;
