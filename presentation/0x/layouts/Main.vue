fb6d5b618c102a28df947a72e2be74921a3f7e60
igdev <dorultanianos@gmail.com>
2025-11-20 08:17:06 +0200

    fix(presentation): missing layout & component

---
+++ presentation/0x/layouts/Main.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Logo from '../components/logo.vue';
+
+</script>
+
+<template>
+  <div class="text-left p-4">
+    <Logo/>
+    <div class="slidev-layout default flex-column justify-between">
+      <slot />
+    </div>
+
+    <Edit/>
+  </div>
+</template>
