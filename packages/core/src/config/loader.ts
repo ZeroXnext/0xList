cef3ef2fe77ec75598157ff405636c594e23f848
igdev <dorultanianos@gmail.com>
2025-11-20 12:36:49 +0200

    feat(core): allow loader to load from config obj or imported from given string

--- packages/core/src/config/loader.ts
+++ packages/core/src/config/loader.ts
@@ -1,12 +1,18 @@
-import {Config} from '@types';
+import { Config } from '@types';
 import defaultConfig from './default';
 
-async function loader(configName = "0xlist.config"): Promise<Config> {
+async function loader(cfg: string | Config): Promise<Config> {
+
   let config: Partial<Config>;
-  try {
-    config = {...defaultConfig, ...(await import(configName) ?? {}).default as Config};
-  } catch (error) {
-    config = defaultConfig;
+  if (typeof cfg === "string") {
+    try {
+      config = { ...defaultConfig, ...(await import(cfg ?? "0xlist.config") ?? {}).default as Config };
+
+    } catch (error) {
+      config = defaultConfig;
+    }
+  } else {
+    config = cfg;
   }
 
   return config as Config;
