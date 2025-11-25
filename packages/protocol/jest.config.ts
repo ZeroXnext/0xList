b0c7387b1640ab65a21ac683fec40f249214973f
igdev <dorultanianos@gmail.com>
2025-11-25 08:59:32 +0200

    feat: create new package "protocol"

---
+++ packages/protocol/jest.config.ts
@@ -0,0 +1,24 @@
+import { Config } from 'jest';
+import { createDefaultPreset } from 'ts-jest';
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+export default {
+  testEnvironment: 'node',
+  rootDir: '.',
+  passWithNoTests: true,
+  testMatch: ['/**/*.test.ts'],
+  moduleNameMapper: {
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@utils$': '<rootDir>/src/utils/index.ts',
+  },
+  transform: {
+    ...tsJestTransformCfg,
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: './tsconfig.jest.json',
+      },
+    ],
+  },
+} as Config;
---
+++ packages/protocol/rollup.config.js
@@ -0,0 +1,53 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import alias from '@rollup/plugin-alias';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import esbuild from 'rollup-plugin-esbuild';
+import json from '@rollup/plugin-json';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+    },
+  ],
+  plugins: [
+    json(),
+    alias({
+      entries: [
+        {
+          find: '@utils',
+          replacement: path.resolve(__dirname, 'src/utils/index.ts'),
+        },
+      ],
+    }),
+    esbuild({
+      // All options are optional
+      include: /\.[jt]sx?$/, // default, inferred from `loaders` option
+      exclude: /node_modules/, // default
+      sourceMap: true, // default
+      minify: import.meta.env?.NODE_ENV === 'production',
+      target: 'esnext', // default, or 'es20XX', 'esnext'
+      jsx: 'transform', // default, or 'preserve'
+      define: {
+        __VERSION__: '"x.y.z"',
+      },
+      tsconfig: 'tsconfig.json', // default
+      // Add extra loaders
+      loaders: {
+        // Add .json files support
+        // require @rollup/plugin-commonjs
+        '.json': 'json',
+        // Enable JSX in .js files too
+        '.js': 'jsx',
+      },
+    }),
+    nodeResolve({ browser: true }),
+  ],
+  ignore: ['src/**/*.test.ts'],
+};
